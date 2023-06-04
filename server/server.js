07e0d7d29d28263f43bec7ca2ac3494833ea7967
Quan <quan.dn95@gmail.com>
2023-06-03 22:15:02 -0700

    config

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import connectDb from './src/config/connectDb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', (req, res) => {
+  res.send('App is running');
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
